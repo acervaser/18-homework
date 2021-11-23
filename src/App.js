6faa365784ee73efe56e0d01f3b1906bc1570f4d
vasyl_artus <basa94389@gmail.com>
2021-11-23 18:46:42 +0200

    anakin add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Yoda from "./img/Yoda.jpg"
 import './App.css';
-
+ const y = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg"
 function App() {
   return (
     <div className="App">
      <h1>18-homework</h1>
      <h2>Author: Vasyl Artus</h2>
-     <img srs={Yoda} alt="yoda"/>
+     <img srs={y} alt="yoda"/>
     </div>
   );
 }
