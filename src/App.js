32282d9c8f2eae294dfd4ef3649c0eaf7ab1cd0c
vasyl_artus <basa94389@gmail.com>
2021-11-23 17:32:38 +0200

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Yoda from "./img/Yoda.jpg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>18-homework</h1>
+     <h2>Author: Vasyl Artus</h2>
+     <img srs={Yoda} alt="yoda"/>
     </div>
   );
 }
