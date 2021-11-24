117ca6107d30390b1d9b4c7cc70c8d471322da2a
vasyl_artus <basa94389@gmail.com>
2021-11-24 10:31:35 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,21 @@
 import './App.css';
 import React from "react";
 import UserInfo from "./components/UserInfo";
-import PostStatistics from "./components/PostStatistikcs";
 import Baby from "./assests/img/baby_yoda.jpg";
 import Yoda from "./assests/img/Yoda.jpg";
 
-const BABY_IMAGE = Baby;
-
-const YODA_IMAGE = Yoda;
-
-
-
-
 const userData = {
     name: "Baby Yoda",
-    photo: BABY_IMAGE,
+    photo: Baby,
     nickname: "@baby_yoda"
 };
 
 const postData = {
     content: "My dear father, I miss you so much...",
-    image: YODA_IMAGE,
-    date: "26 feb"
+    image: Yoda,
+    date: "14 nov"
 };
 
-const statisticsData = {
-    comments: 482,
-    shares: 146,
-    likes: 887,
-}
 const App = () => {
     return (
         <div className="post">
@@ -38,9 +25,7 @@ const App = () => {
                 <span className="post-date">{postData.date}</span>
                 <span className="post-content">{postData.content}</span>
             </div>
-
             <img className="post-image" src={postData.image} alt="Some post" />
-            <PostStatistics comments={statisticsData.comments} shares={statisticsData.shares} likes={statisticsData.likes} />
         </div>
     );
 };
