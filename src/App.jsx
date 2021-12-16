ee8d4026518287d521d99328d7a708e6cac9bb4d
vasyl_artus <basa94389@gmail.com>
2021-12-16 19:21:17 +0200

    done++

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
 
 import './App.css';
 import React from "react";
-import UserInfo from "./components/UserInfo";
-import Baby from "./assests/img/baby_yoda.jpg";
-import Yoda from "./assests/img/Yoda.jpg";
-
-const userData = {
-    name: "Baby Yoda",
-    photo: Baby,
-    nickname: "@baby_yoda"
-};
-
-const postData = {
-    content: "My dear father, I miss you so much...",
-    image: Yoda,
-    date: "14 nov"
-};
+import Post from './components/Post';
 
 const App = () => {
     return (
-        <div className="post">
-            <div className="wrapper">
-                <UserInfo name={userData.name} image={userData.photo} nickname={userData.nickname} />
-                <span className="post-date">{postData.date}</span>
-                <span className="post-content">{postData.content}</span>
-            </div>
-            <img className="post-image" src={postData.image} alt="Some post" />
-        </div>
+        <Post></Post>
     );
 };
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+import Baby from "../assests/img/baby_yoda.jpg";
+import Yoda from "../assests/img/Yoda.jpg";
+
+const userData = {
+    name: "Baby Yoda",
+    photo: Baby,
+    nickname: "@baby_yoda"
+};
+
+let day = new Date;
+const postData = {
+    content: "My dear father, I miss you so much...",
+    image: Yoda,
+    date: day.getDate()
+};
+
+const Post = () => {
+    return (
+        <div className="post">
+            <div className="wrapper">
+                <UserInfo name={userData.name} image={userData.photo} nickname={userData.nickname} />
+                <span className="post-date">{postData.date}</span>
+                <span className="post-content">{postData.content}</span>
+            </div>
+            <img className="post-image" src={postData.image} alt="Some post" />
+        </div>
+    );
+};
+
+export default Post
